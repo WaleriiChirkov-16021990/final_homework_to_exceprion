a5973df5021b1359397a64316bde16debbc32359
Валерий Чирков <zx84058405@live.com>
2023-04-18 00:05:18 +0300

    Финальная версия приложения

--- src/Model/Exit_application.java
+++ src/Model/Exit_application.java
@@ -5,10 +5,41 @@ import View.Console_terminal.Printer;
 
 import java.util.Scanner;
 
+/**
+ * Класс дает возможность прекратить работу приложжения в штатном режиме,
+ * если что то пошло не так, и по собственному желанию.
+ */
 public class Exit_application {
-	public boolean select_user(Scanner scanner) {
-		Printer.to_print(Printer.exit);
-		String string = new Inputer_for_console(scanner).u_input();
+	private static int counter_input = 5;
+	
+	/**
+	 * Данный метод принимает внешний инициализированный сканер, далее обрабатывает ввод юзера в консоль,
+	 * информируя его о последствиях данного ввода,
+	 * и на основе этого пропускает его на дальнейший ввод данных или завершает работу приложения.
+	 * @param scanner - инициализированный сканер
+	 * @return истина или ложь
+	 */
+	public boolean select_user(Scanner scanner) throws RuntimeException {
+		if (scanner == null) {
+			throw new RuntimeException(Printer.not_scanner);
+		}
+		String string=null;
+		while (counter_input > 0) {
+			Printer.to_print(Printer.exit);
+			string = new Inputer_for_console(scanner).u_input();
+			if (string.equals(Printer.to_exit) || string.equals(Printer.to_enter_user)) {
+				break;
+			} else {
+				Printer.to_print(Printer.to_enter_user_error);
+			}
+			counter_input--;
+			Printer.to_print(Printer.to_exit_count + counter_input);
+			if (counter_input == 0) {
+				Printer.to_print(Printer.getTo_exit_);
+				return false;
+			}
+			Printer.to_print(Printer.repeate_to_enter);
+		}
 		if (string.replace(" ", "").equals(Printer.to_exit)) {
 			return false;
 		}
--- src/Model/User.java
+++ src/Model/User.java
@@ -2,11 +2,19 @@ package Model;
 
 import Model.Abstract.Human;
 
+/**
+ * Класс описывает юзера, который создастся после успешного ввода всех параметров.
+ */
 public class User extends Human {
 	private String date;
 	private String telephone_number;
 	private String gender;
 	
+	/**
+	 * Конструктор
+	 * @param full_name Массив строк, это полноценный массив из 6 параметров юзера,
+	 *                  хранящий в себе 6 параметров требуемого типа, формата и значения.
+	 */
 	public User(String[] full_name) {
 		super(full_name);
 		this.date = full_name[3];
@@ -14,6 +22,15 @@ public class User extends Human {
 		this.gender = full_name[5];
 	}
 	
+	/**
+	 * Конструктор
+	 * @param last_name - Фамилия
+	 * @param first_name - Имя
+	 * @param patronymic - Отчество
+	 * @param date - Дата рождения
+	 * @param telephone_number - Номер мобильного телефона
+	 * @param gender - половая принадлежность.
+	 */
 	public User(String last_name, String first_name, String patronymic, String date, String telephone_number, String gender) {
 		super(last_name, first_name, patronymic);
 		this.date = date;
@@ -36,6 +53,11 @@ public class User extends Human {
 		this.telephone_number = number;
 	}
 	
+	/**
+	 *  Данный метод преобразует параметры юзера в строку, для дальнейшей передачи информации,
+	 *  или например тут я использую его для записи в файл.
+	 * @return Строка параметров человека перечисленных через пробел.
+	 */
 	@Override
 	public String toString() {
 		return getLast_name() + " " +
